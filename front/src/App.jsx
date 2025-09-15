121c5b7340f956285b1b344da87e19114d33b3c5
wonJoon <jee861203@gmail.com>
2025-09-15 16:01:17 +0900

    setting

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
 import './App.css'
-import { Routes, Route, Link } from "react-router-dom"
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom"
+import Main from "./pages/Main.jsx";
 
 function App() {
 
+    useEffect(() => {
+        fetch("http://localhost:8080/react/todos")
+            .then(res => res.json())
+            .then(data => setTodos(data))
+            .catch(err => console.log(err));
+    },[])
+
 
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Main/>}></Route>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ front/src/components/UserHeader.jsx
@@ -0,0 +1,5 @@
+const UserHeader = () => {
+    return<></>
+}
+
+export default UserHeader;
