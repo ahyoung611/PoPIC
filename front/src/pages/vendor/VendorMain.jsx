63b460cac40f594a544091e25b06969c800f45f1
ahyoung611 <wlq0611@naver.com>
2025-09-16 23:37:17 +0900

    공통 컴포넌트 confirm 모달창 구현

--- front/src/pages/vendor/VendorMain.jsx
+++ front/src/pages/vendor/VendorMain.jsx
@@ -2,30 +2,51 @@ import React, { useEffect, useState } from "react";
 import SearchHeader from "../../components/commons/SearchHeader";
 import Pagination from "../../components/commons/Pagination";
 import Select from "../../components/commons/Select.jsx";
+import Button from "../../components/commons/Button.jsx";
+import ConfirmModal from "../../components/commons/ConfirmModal.jsx";
 
 export default function VendorMain() {
     const [searchValue, setSearchValue] = useState("");
-    const [sort, setSort] = useState("");           // ✅ 정렬 상태 추가
+    const [sort, setSort] = useState("");
     const [currentPage, setCurrentPage] = useState(1);
     const totalPages = 5;
 
     const handleSearch = () => {
         console.log("검색 실행:", { searchValue, sort });
-        // TODO: API 호출 or 필터링
     };
 
     const handleRegister = () => {
         console.log("등록 버튼 클릭");
     };
 
-    // ✅ 검색/정렬 변경 시 1페이지로 리셋
     useEffect(() => {
         setCurrentPage(1);
     }, [searchValue, sort]);
 
+    const [open, setOpen] = useState(false);
+    const [title, setTitle] = useState("반려하시겠습니까?");
+
+    const handleReject = () => {
+        // TODO: 반려 처리(API 등)
+        setOpen(false);
+    };
+
     return (
         <div className="container">
             <div className="inner">
+                <Button variant="outline" className="rounded-xl" onClick={() => setOpen(true)}>
+                    반려
+                </Button>
+
+                <ConfirmModal
+                    open={open}
+                    title="확인하시겠습니까?" // 타이틀 변경
+                    okText="네"
+                    cancelText="아니오"
+                    danger
+                    onConfirm={handleReject}
+                    onCancel={() => setOpen(false)}
+                />
 
                 {/* 정렬 셀렉트 */}
                 <Select
