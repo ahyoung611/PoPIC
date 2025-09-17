293c9c7e7da1805f4314f922120d8c1bf0bc17e1
jihyun <jhbuse0612@gmail.com>
2025-09-17 11:29:29 +0900

    게시판 리스트 구현 중 ...

--- front/src/pages/user/BoardList.jsx
+++ front/src/pages/user/BoardList.jsx
@@ -1,14 +1,15 @@
-import BoardEditor from "./BoardEditor.jsx";
 import {useNavigate} from "react-router-dom";
 import Button from "../../components/commons/Button.jsx";
+import BoardListView from "../../components/board/BoardListItem.jsx";
 
-const BoardList = ()=>{
+const BoardList = () => {
     const nav = useNavigate();
 
-    return(
+    return (
         <div>
-            리스트
-            <Button onClick={ ()=>{ nav("/board/new")}  }>글 작성</Button>
+            <h2>게시판</h2>
+            <Button onClick={() => nav("/board/new")}>글 작성</Button>
+            <BoardListView/>
         </div>
     )
 }
