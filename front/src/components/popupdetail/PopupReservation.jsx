28b9e948b03c90140a6bbdd5c44c85bcd0ce4f57
jihyun <jhbuse0612@gmail.com>
2025-09-19 14:23:13 +0900

    현장 대기 모달 수정

--- front/src/components/popupdetail/PopupReservation.jsx
+++ front/src/components/popupdetail/PopupReservation.jsx
@@ -1,21 +1,27 @@
 import {useState} from "react";
 
-const PopupReservation = (props)=>{
+const PopupReservation = (props) => {
     const [reservationNumber, setReservationNumber] = useState(1);
     const [reservationDate, setReservationDate] = useState("");
     const [reservationTime, setReservationTime] = useState("");
 
-    function reservationSubmit(){
+    function reservationSubmit() {
         // 예약 버튼을 클릭했을 때 모달에 정보를 전달
         const reservationData = {
-            name: props.popup?.name,
+            name: props.popup.store_name,
             date: reservationDate,
             time: reservationTime,
             price: props.popup?.price,
             numberOfPeople: reservationNumber,
         };
         console.log("예약 데이터:", reservationData);
-        props.onOpenModal(reservationData); // 모달 열기 및 예약 데이터 전달
+        if (reservationDate == "") {
+            alert("예약 날짜를 선택해주세요.");
+        } else if (reservationTime == "") {
+            alert("예약 시간을 선택해주세요.");
+        } else {
+            props.onOpenModal(reservationData); // 모달 열기 및 예약 데이터 전달
+        }
     }
 
     return (
@@ -28,21 +34,23 @@ const PopupReservation = (props)=>{
 
             <div className={"reservation-number-input"}>
                 <p>입장 예약</p>
-                <button className={"minus"} onClick={()=>{
-                    if(reservationNumber > 1){
-                        setReservationNumber(prev=> prev-1);
-                    }else{
+                <button className={"minus"} onClick={() => {
+                    if (reservationNumber > 1) {
+                        setReservationNumber(prev => prev - 1);
+                    } else {
                         alert("최소 1명 이상 예약 가능합니다");
                     }
-                }}>-</button>
+                }}>-
+                </button>
                 <span>{reservationNumber}명</span>
-                <button className={"plus"} onClick={()=>{
-                    if(reservationNumber <= 1){
-                        setReservationNumber(prev=> prev+1);
-                    }else{
+                <button className={"plus"} onClick={() => {
+                    if (reservationNumber <= 1) {
+                        setReservationNumber(prev => prev + 1);
+                    } else {
                         alert("최대 2명 까지 예약 가능합니다");
                     }
-                }}>+</button>
+                }}>+
+                </button>
             </div>
 
             <div className={"reservation-submit"}>
