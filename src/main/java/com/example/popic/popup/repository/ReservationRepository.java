4d3ee86ac10c4729ab558e23b8b03e471ec31a2c
jihyun <jhbuse0612@gmail.com>
2025-09-22 11:41:03 +0900

    결제 구현 중

--- src/main/java/com/example/popic/popup/repository/ReservationRepository.java
+++ src/main/java/com/example/popic/popup/repository/ReservationRepository.java
@@ -4,6 +4,7 @@ import com.example.popic.entity.entities.Reservation;
 import com.example.popic.popup.dto.PopupReservationDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,5 +12,8 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     @Query("SELECT r FROM Reservation r WHERE r.store.store_id = :popupId")
     List<Reservation> getReservationsByPopupId(Long popupId);
+
+    @Query("SELECT r FROM Reservation r WHERE r.user.user_id = :userId")
+    List<Reservation> findByUserId(@Param("userId") Long userId);
 
 }
--- src/main/java/com/example/popic/reservation/ReservationController.java
+++ src/main/java/com/example/popic/reservation/ReservationController.java
@@ -1,5 +1,6 @@
 package com.example.popic.reservation;
 
+import com.example.popic.popup.dto.PopupReservationDTO;
 import com.example.popic.reservation.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,21 +17,16 @@ public class ReservationController {
 
     // 결제 확인 + 예약 저장
     @PostMapping("/confirm")
-    public ResponseEntity<ReservationDTO> confirmAndSave(@RequestBody ReservationDTO dto) {
-        // TODO: 토스 결제 confirm API 호출 후 성공 시 저장
-        ReservationDTO saved = reservationService.saveReservation(dto);
+    public ResponseEntity<PopupReservationDTO> confirmAndSave(@RequestBody PopupReservationDTO dto) {
+        // 토스 결제 confirm API 호출 후 성공 시 저장
+        PopupReservationDTO saved = reservationService.saveReservation(dto);
         return ResponseEntity.ok(saved);
     }
 
     // 사용자 예약 조회
     @GetMapping("/user/{userId}")
-    public List<ReservationDTO> getUserReservations(@PathVariable Long userId) {
-        return reservationService.getUserReservations(userId);
-    }
-
-    // 운영자 전체 예약 조회
-    @GetMapping
-    public List<ReservationDTO> getAllReservations() {
-        return reservationService.getAllReservations();
+    public ResponseEntity<List<PopupReservationDTO>> getUserReservations(@PathVariable Long userId) {
+        List<PopupReservationDTO> reservations = reservationService.getUserReservations(userId);
+        return ResponseEntity.ok(reservations);
     }
 }
