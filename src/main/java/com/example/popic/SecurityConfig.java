cd77c6f318913bef39adfe883affdaa575102107
wonJoon <jee861203@gmail.com>
2025-09-24 17:44:25 +0900

    팝업스토어 qr 체크시 SSE기반 실시간 입장 처리

--- src/main/java/com/example/popic/SecurityConfig.java
+++ src/main/java/com/example/popic/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig {
                 .cors(cors -> cors.configure(http)) // CORS 설정
                 .csrf(csrf -> csrf.disable()) // CSRF 비활성화
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user/login", "/auth/refresh", "/user/join", "/vendor/login", "/vendor/join", "/admin/login", "/auth/**", "/images", "/scan-qr", "/", "/login", "/board/file/**").permitAll() // 로그인/회원가입/토큰 갱신은 허용
+                        .requestMatchers("/user/login", "/auth/refresh", "/user/join", "/vendor/login", "/vendor/join", "/admin/login", "/auth/**", "/images", "/scan-qr",
+                                "/", "/login", "/board/file/**", "/popupStore/monthly","/qr-stream").permitAll() // 로그인/회원가입/토큰 갱신은 허용
                         .anyRequest().authenticated() // 나머지는 인증 필요
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
