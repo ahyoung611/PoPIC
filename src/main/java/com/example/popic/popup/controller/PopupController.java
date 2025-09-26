e37ed90649847409ec5f06dd77eca8b470fa63bd
wonJoon <jee861203@gmail.com>
2025-09-26 09:56:08 +0900

    로그인 한 사용자 권한에 따라 헤더 변경

--- src/main/java/com/example/popic/popup/controller/PopupController.java
+++ src/main/java/com/example/popic/popup/controller/PopupController.java
@@ -102,13 +102,15 @@ public class PopupController {
     }
 
     @PostMapping("/inquiryReply")
-    public ResponseEntity<?> createReply( @RequestBody InquiryRepliyDTO reply, Authentication authentication) {
+    public ResponseEntity<?> createReply( @RequestBody InquiryRepliyDTO reply,
+                                          Authentication authentication) {
 
         // 로그인 벤더 정보 가져오기
         CustomUserPrincipal principal = (CustomUserPrincipal) authentication.getPrincipal();
         System.out.println("principal" + principal);
 
         reply.setVendor(new VendorDTO(vendorRepository.findById(principal.getId()).orElse(null)));
+
 
         inquiryService.saveReply(reply);
 
